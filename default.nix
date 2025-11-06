ccb420d050e5a46ab433dfb97431a0cd30e3dec0
GitHub Action <action@github.com>
2025-11-06 17:19:46 +0000

    Updating 1.78.2 -> 1.78.3

--- default.nix
+++ default.nix
@@ -5,10 +5,10 @@
 }:
 let
   pname = "heptabase";
-  version = "1.78.2";
+  version = "1.78.3";
   src = fetchurl {
     url = "https://github.com/heptameta/project-meta/releases/download/v${version}/Heptabase-${version}.AppImage";
-    sha256 = "b607c1e3d2bd95c5f6bb87a3710e3e96b8b82f0a0c38da9218f8ef4cb84322c0";
+    sha256 = "8cdb8d36c913a87d3fec4af72b4c91fee45177618433da592163800468a0e267";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
