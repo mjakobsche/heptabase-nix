8597928b5d297425aba7e25ffb7e9269c4728454
GitHub Action <action@github.com>
2026-05-27 16:37:57 +0000

    Updating null -> 1.96.3

--- default.nix
+++ default.nix
@@ -5,10 +5,10 @@
 }:
 let
   pname = "heptabase";
-  version = "null";
+  version = "1.96.3";
   src = fetchurl {
     url = "https://github.com/heptameta/project-meta/releases/download/v${version}/Heptabase-${version}.AppImage";
-    sha256 = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5";
+    sha256 = "a995c5a92075a3bb63c7165fd6ecedff79ad4ca2e0d5400070bf67d23bf73a2f";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
